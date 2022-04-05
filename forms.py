8d041055f89b30cc6c354162c977887d1a8312c1
coder6497 <92335331+coder6497@users.noreply.github.com>
2022-04-05 22:49:40 +0300

    Added addition to SQLAlchemy data_base

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
-
-
-class Form(FlaskForm):
-    title = StringField("Заголовок", validators=[DataRequired()])
-    text = TextAreaField("Текст", validators=[DataRequired()])
-    submit = SubmitField("Сохранить")
---
+++ notes.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes_base.db'
+db = SQLAlchemy(app)
+
+
+class Form(FlaskForm):
+    title = StringField("Заголовок", validators=[DataRequired()])
+    text = TextAreaField("Текст", validators=[DataRequired()])
+    submit = SubmitField("Сохранить")
+
+
+class Notes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String())
+    text = db.Column(db.String(120))
+
+    def __repr__(self):
+        return "<Notes {}r>".format(self.id)
+
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+
+@app.route('/new_form', methods=['POST', 'GET'])
+def new_form():
+    form = Form()
+    if form.validate_on_submit():
+        title = form.title.data
+        text = form.text.data
+        data = Notes(title=title, text=text)
+        db.session.add(data)
+        db.session.commit()
+    return render_template('new_form.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
